b84846263d402c0f5b2ac3748810fbc0b686986a
rajat <rajat@rajatmacbook.local>
2024-05-31 10:45:10 +0530

    Redirection added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/blog/sitemap.xml",
+        destination:
+          "https://blogs-theta-six.vercel.app/blog/blog.boilercode.app/sitemap.xml",
+      },
+      {
+        source: "/blog",
+        destination:
+          "https://blogs-theta-six.vercel.app/blog/blog.boilercode.app",
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://blogs-theta-six.vercel.app/blog/:path*",
+      },
+      {
+        source: "/blog/blog-sitemap.xml",
+        destination:
+          "https://blogs-theta-six.vercel.app/blog/blog.boilercode.app/sitemap.xml",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
